2259b4d09075cf370372b12e757a45c9ea2b149e
estebanrfp <estebanrfp@gmail.com>
2016-12-17 17:40:35 -0300

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    resolve: {
+        extensions: ['', '.js']
+    },
+    context: __dirname,
+    entry: {
+        app:'./src/index.js'
+    },
+    output: {
+        path: './build',
+        filename: '[name].js',
+        publicPath: '/build/'
+    },
+    resolve: {
+        extensions: ['', '.js']
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+        inline: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /(\.js|.jsx)$/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'stage-2']
+                }
+            }
+        ]
+    }
+}
