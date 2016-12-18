68dc93c5947847e2a69a83408e600f29890a7e9c
estebanrfp <estebanrfp@gmail.com>
2016-12-17 22:41:48 -0300

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,30 @@
 module.exports = {
-    resolve: {
-        extensions: ['', '.js']
-    },
-    context: __dirname,
-    entry: {
-        app:'./src/index.js'
-    },
-    output: {
-        path: './dist',
-        filename: '[name].js',
-        publicPath: '/build/'
-    },
-    resolve: {
-        extensions: ['', '.js']
-    },
-    devServer: {
-        host: '0.0.0.0',
-        port: 8080,
-        inline: true
-    },
-    module: {
-        loaders: [
-            {
-                test: /(\.js|.jsx)$/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'stage-2']
-                }
-            }
-        ]
-    }
+  resolve: {
+    extensions: ['', '.js']
+  },
+  context: __dirname,
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    path: './dist',
+    filename: '[name].bundle.js',
+    publicPath: '/dist/'
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    inline: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.js|.jsx)$/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-2']
+        }
+      }
+    ]
+  }
 }
